4b58dea959697b70cf5280e545f67fe4f30b31ed
gasgithub <gasgithub@users.noreply.github.com>
2024-02-19 18:33:40 +0100

    notification fixes

--- apic-config/scripts/apic-setup-notifications.sh
+++ apic-config/scripts/apic-setup-notifications.sh
@@ -33,20 +33,13 @@ echo " "
 echo "Creating Mail Server in APIC using generated ${APIC_MAIL_SERVER}.json file..."
 apic mail-servers:create -s $APIC_MGMT_SERVER -o $APIC_ADMIN_ORG --format json "${APIC_MAIL_SERVER}.json" > ${APIC_MAIL_SERVER}-create-output.json
 
-echo "  Testing Connection to Mail Server just created in APIC..."
-echo "  Generate Test Email Recipient File"
-jq -n --arg email "test@${CUSTOMER_NAME}.com" '{"recipients": [$email]}'\
-  > "${CUSTOMER_NAME}-mail-test.json"
-
-echo "  Test Nofication will be sent to - test@${CUSTOMER_NAME}.com"
-apic mail-servers:test-connection --server $APIC_MGMT_SERVER --org $APIC_ADMIN_ORG --format json ${APIC_MAIL_SERVER} ${CUSTOMER_NAME}-mail-test.json
 
 #################################################
 # Update Cloud Settings to use new Email Server #
 #################################################
 echo " "
 echo "Get current cloud settings and store in cloud-settings-original.json file..."
-apic cloud-settings:get -s $APIC_MGMT_SERVER --format json --fields mail_server_url --output - > cloud-setting-ms-original.json
+#apic cloud-settings:get -s $APIC_MGMT_SERVER --format json --fields mail_server_url --output - > cloud-setting-ms-original.json
 
 echo "  Get the URL for the new mail server..."
 MAIL_SERVER_URL=$(cat ${APIC_MAIL_SERVER}-create-output.json | jq -r '.url')
@@ -55,7 +48,15 @@ echo "  New Mail Server URL - $MAIL_SERVER_URL"
 echo "  Update New Mail Server URL and save in cloud-settings-ms-updated.json file"
 jq --arg MAIL_SERVER_URL "$MAIL_SERVER_URL" \
   '.mail_server_url=$MAIL_SERVER_URL'\
-  cloud-setting-ms-original.json > "cloud-setting-ms-updated.json"
+  cloud-setting-template.json > "cloud-setting-ms-updated.json"
 
 echo "  Add the New Mail Server to the cloud settings in APIC"
 apic cloud-settings:update -s $APIC_MGMT_SERVER --format json cloud-setting-ms-updated.json > cloud-setting-update-output.json
+
+echo "  Testing Connection to Mail Server just created in APIC..."
+echo "  Generate Test Email Recipient File"
+jq -n --arg email "test@${CUSTOMER_NAME}.com" '{"recipients": [$email]}'\
+  > "${CUSTOMER_NAME}-mail-test.json"
+
+echo "  Test Nofication will be sent to - test@${CUSTOMER_NAME}.com"
+apic mail-servers:test-connection --server $APIC_MGMT_SERVER --org $APIC_ADMIN_ORG --format json ${APIC_MAIL_SERVER} ${CUSTOMER_NAME}-mail-test.json
