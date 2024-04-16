a256d39b5c7d99263e70d55efb490c2e21d899ed
gasgithub <gasgithub@users.noreply.github.com>
2024-04-16 09:32:33 +0200

    dmc fix update

--- cp4d/cp4d-pipeline-run/dmc-fix.sh
+++ cp4d/cp4d-pipeline-run/dmc-fix.sh
@@ -3,10 +3,10 @@ export CPD_ROUTE=$(oc get route cpd -n cpd -o jsonpath="{.spec.host}")
 
 export CPD_ADMIN_PASSWORD=$(oc get secret "admin-user-details" -n cpd -o jsonpath="{.data.initial_admin_password}"| base64 -d)
 
-export CPD_TOKEN=$(curl --location --request POST "https://$CPD_ROUTE/v1/preauth/signin" --header 'Content-Type: application/json' --data-raw "{\"username\":\"admin\",\"password\":\"$CPD_ADMIN_PASSWORD\"}" | jq -r .token)
+export CPD_TOKEN=$(curl -k --location --request POST "https://$CPD_ROUTE/v1/preauth/signin" --header 'Content-Type: application/json' --data-raw "{\"username\":\"admin\",\"password\":\"$CPD_ADMIN_PASSWORD\"}" | jq -r .token)
 
 # check dmc state
-curl --location --request POST "https://$CPD_ROUTE/zen-data/v1/addOn/query" \
+curl -k --location --request POST "https://$CPD_ROUTE/zen-data/v1/addOn/query" \
 --header "Authorization: Bearer $CPD_TOKEN" \
 --header 'Content-Type: application/json' \
 --data-raw '{
@@ -15,20 +15,20 @@ curl --location --request POST "https://$CPD_ROUTE/zen-data/v1/addOn/query" \
 
 
 # get dmc id
-export DMC_ID=$(curl --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="dmc").id')
+export DMC_ID=$(curl -k --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="dmc").id')
 
 # get db2oltp id
-export DB2OLT_ID=$(curl --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="db2oltp").id')
+export DB2OLT_ID=$(curl -k --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="db2oltp").id')
 
 # get db2oltp display name - OTHERWISE IT NOT WORKS!!!!
-export DB2OLT_DISPLAY_NAME=$(curl --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="db2oltp").display_name')
+export DB2OLT_DISPLAY_NAME=$(curl -k --location --request GET "https://$CPD_ROUTE/zen-data/v3/service_instances?fetch_all_instances=true" --header "Authorization: Bearer $CPD_TOKEN"  | jq -r '.service_instances[] | select(.addon_type =="db2oltp").display_name')
 
 
 # get the db2 password
 export DB2_PASSWORD=$(oc get secret "c-db2oltp-$DB2OLT_ID-instancepassword" -n cpd -o jsonpath="{.data.password}"| base64 -d)
 
 
-curl --location --request POST "https://$CPD_ROUTE/addon-dmc/v1/profiles" \
+curl -k --location --request POST "https://$CPD_ROUTE/addon-dmc/v1/profiles" \
 --header "Authorization: Bearer $CPD_TOKEN" \
 --header 'Content-Type: application/json' \
 --data-raw "{
